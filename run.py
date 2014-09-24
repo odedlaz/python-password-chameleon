8055c0a0e6725458a70b9b0ee90e89f4a82c9e79
Oded Lazar <odedlaz@gmail.com>
2014-09-25 01:00:53 +0300

    moved to argparse and made the copy to clipboard optional

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import hashlib
 import base64
 from getpass import getpass
 import sys
-import pyperclip
+import argparse
 charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
 chameleon_charset = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz123456789?!#@&$="
 numbers = "123456789"
@@ -13,9 +13,9 @@ letters = "ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 punct = "?!#@&$"
 
 def hashify(item):
-	m = hashlib.sha1()
-	m.update(item)
-	return m.digest()
+    m = hashlib.sha1()
+    m.update(item)
+    return m.digest()
 
 def generate(secretpassword, sitename):
     chained = "{0}:{1}".format(secretpassword,sitename.lower())
@@ -26,49 +26,56 @@ def generate(secretpassword, sitename):
     return pwd
 
 def change_encoding(s):
-	encoded = ""
-	for character in s:
-		index = charset.index(character)
-		encoded = encoded + chameleon_charset[index]
-	return encoded
+    encoded = ""
+    for character in s:
+        index = charset.index(character)
+        encoded = encoded + chameleon_charset[index]
+    return encoded
 
 def ensurenumberandletter(s):
-	
     hasnumber = False
     hasletter = False
     haspunct = False
 
     for character in s:
-    	if character in numbers:
-    		hasnumber = True
-    	if character in letters:
-    		hasletter = True
-    	if character in punct:
-    		haspunct = True
+        if character in numbers:
+            hasnumber = True
+        if character in letters:
+            hasletter = True
+        if character in punct:
+            haspunct = True
 
     if not hasnumber:
         s = "1" + s[1:]
     if not hasletter:
-    	s = s[:1] + "a" + s[2:]
+        s = s[:1] + "a" + s[2:]
     if not haspunct:
-    	s = s[:2] + "@" + s[3:]
+        s = s[:2] + "@" + s[3:]
     return s
 
-def main(args):
-    if len(args) != 2:
-        print("invalid arguments. usage: {0} <hostname>".format(sys.argv[0]))
-        return
+def copy_passwd_to_clipboard(passwd):
+    try:
+        import pyperclip
+        pyperclip.copy(passwd)
+    except ImportError: 
+        print("cannot copy to clipboard because the pyperclip package is not installed.")
 
-    _,hostname = tuple(args)
-    print("generating password for hostname: {0}".format(hostname))
+def main(args):
+    print("generating password for site: {0}".format(args.sitename))
     master_passwd = getpass("enter the master password: ")
-    generated_passwd = generate(master_passwd, hostname)
-    print("generated password: {} (copied to clipboard)".format(generated_passwd))
-    pyperclip.copy(generated_passwd)
+    generated_passwd = generate(master_passwd, args.sitename)
+    print("generated password: {}".format(generated_passwd))
+    if args.copy:
+        copy_passwd_to_clipboard(generated_passwd)
+
 
 if __name__ == "__main__":
     try:    
-        main(sys.argv)
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-n","--sitename", help="the sitename to generated password to", type=str, required=True)
+        parser.add_argument("-c","--copy", help="copy to clipboard", action="store_true", default=False)
+        args = parser.parse_args()
+        main(args)
     except KeyboardInterrupt: 
-        print("\nbye bye!")
+        print("\nbye!")
         pass
